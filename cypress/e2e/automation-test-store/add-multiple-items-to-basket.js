89a428d2668984c73b8057af7c4689f4561da2d2
Liban <libanhqa@gmail.com>
2024-07-14 12:05:05 +0100

    Second commit

--- cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
@@ -1,7 +1,7 @@
 import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/Autostore_Homepage_PO";
 import HairCare_PO from "../../support/pageObjects/automation-test-store/HairCare_PO";
 ///<reference types= 'cypress'/>
-describe('Add multiple items to basket', () => {
+describe('Add multiple items to the basket', () => {
     const homePage = new AutoStore_Homepage_PO()
     const hairCarePage = new HairCare_PO()
 
